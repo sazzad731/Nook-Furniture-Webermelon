149401de720bbb1153a7fe2a2f363c92d44a1ff4
sazzad731 <sajjadho731@gmail.com>
2026-07-16 17:17:31 +0600

    [DONE]: projet done partialy

--- index.js
+++ index.js
@@ -2,27 +2,64 @@ import Swiper from "https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.mjs
 
 
 
-const track = document.getElementById("tickerTrack");
+document.addEventListener("DOMContentLoaded", function(){
+  const track = document.getElementById("tickerTrack");
 
-const clone = track.innerHTML;
-track.innerHTML += clone;
+  const clone = track.innerHTML;
+  track.innerHTML += clone;
 
-let speed = 1;
-let position = 0;
+  let speed = 1;
+  let position = 0;
 
-function scrollTicker() {
-  position -= speed;
+  function scrollTicker() {
+    position -= speed;
 
-  if (Math.abs(position) >= track.scrollWidth / 2) {
-    position = 0;
+    if (Math.abs(position) >= track.scrollWidth / 2) {
+      position = 0;
+    }
+
+    track.style.transform = `translateX(${position}px)`;
+
+    requestAnimationFrame(scrollTicker);
   }
 
-  track.style.transform = `translateX(${position}px)`;
+  scrollTicker();
 
-  requestAnimationFrame(scrollTicker);
-}
 
-scrollTicker();
+
+
+  // category functionality
+  function handleCategory(){
+    const tabs = document.querySelectorAll(".tab-item")
+    const productCardContainer = document.querySelector(".collection-grid")
+    const productCard = document.querySelectorAll(".collection-grid-item");
+
+    function filterCategory(category){
+      productCard.forEach(product=> {
+        
+        if(category === "all"){
+          product.classList.remove("hide")
+        }else if(product.dataset.category === category){
+          product.classList.remove("hide")
+        }else{
+          product.classList.add("hide")
+        }
+      })
+
+      tabs.forEach(tab=> category === tab.dataset.filter? tab.classList.add("active") :  tab.classList.remove("active"))
+    }
+
+
+    tabs.forEach(tab=>{
+      tab.addEventListener("click", function(){
+        filterCategory(this.dataset.filter);
+      })
+    })
+
+  }
+
+  handleCategory();
+})
 
 
 
